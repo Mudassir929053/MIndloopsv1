4413b21267c53d40e1aea3106556fa3d12525c79
Mohammad Abdul Mudassir <121214686+Mudassir929053@users.noreply.github.com>
2024-09-23 21:35:11 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     if(isset($_SESSION['userType'])){   
         if($_SESSION['userType']=='3'){   
         header('location: index/index.php');  
-    }     
+    }      
     else if($_SESSION['userType']=='2'){  
         header('location: ./Teacher/magazine/index.php'); 
     }  
