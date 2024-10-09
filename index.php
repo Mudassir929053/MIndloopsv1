6dd5d2f7c916ef4ddb1eceba0cc2d208a4b8fa64
Mohammad Abdul Mudassir <121214686+Mudassir929053@users.noreply.github.com>
2024-10-08 20:15:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     }      
     else if($_SESSION['userType']=='2'){  
         header('location: ./Teacher/magazine/index.php'); 
-    }
+    } 
     else if($_SESSION['userType']=='4'){  
         header('location: ./parent/magazine/index.php');
     }
