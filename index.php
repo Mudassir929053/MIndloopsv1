7f0e83b48b3dd26c87a54958b572326056736d79
Mohammad Abdul Mudassir <121214686+Mudassir929053@users.noreply.github.com>
2024-12-15 23:26:30 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php
+ <?php
     session_start();
     // var_dump($_SESSION['userType']);
     // ++!exit();
     if(isset($_SESSION['userType'])){
-        if($_SESSION['userType']=='3'){
+        if($_SESSION['userType']=='3'){ 
         header('location: index/index.php'); 
     }
     else if($_SESSION['userType']=='2'){
